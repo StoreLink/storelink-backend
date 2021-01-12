f0eafafefdee30566582a6fc83dae9e98307373c
Mussa <62203153+MussaOPS@users.noreply.github.com>
2021-01-12 19:33:28 +0600

    Rest GetMapping

--- StoreLink/src/main/java/kz/storelink/StorelinkApplication.java
+++ StoreLink/src/main/java/kz/storelink/StorelinkApplication.java
@@ -2,12 +2,20 @@ package kz.storelink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StorelinkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StorelinkApplication.class, args);
+    }
+
+    @GetMapping
+    public String testFunction() {
+        return "Mukhtar KOTAKBAS!!!!";
     }
 
 }
